1908b85ee9a7dea98cde52a651b3112e6953317e
sayeed083 <sayeedislam407@gmailcom>
2024-12-20 12:59:37 +0600

    home banner half

--- src/Pages/Home/HomeBanner/HomeBanner.jsx
+++ src/Pages/Home/HomeBanner/HomeBanner.jsx
@@ -1,11 +1,75 @@
 import React from 'react';
+import './HomeBanner.css';
 
 const HomeBanner = () => {
     return (
-        <div>
-            <img src="https://i.postimg.cc/vZSPsCWC/hb3.jpg" alt="" />
+        <div className="home-banner">
+            <img src="https://i.postimg.cc/vZSPsCWC/hb3.jpg" alt="Universe Background" className="background-image" />
+            <div className="banner-content mt-10 flex justify-between">
+                <div>
+                    <h2 className='text-5xl font-semibold uppercase'>Explore</h2>
+                    <h1 className='text-7xl font-bold uppercase my-8'>The Universe</h1>
+                    <p className='text-lg font-medium'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam, <br /> voluptatum commodi aut inventore obcaecati veniam.</p>
+                </div>
+                <div>
+                    <h2 className='text-3xl text-end font-bold uppercase '>
+                        Constellation
+                        <p className='text-base font-medium normal-case text-cyan-400'> Duis aute irure dolor in </p>
+                    </h2>
+                    <h2 className='text-3xl text-end font-bold uppercase my-8'>
+                        Constellation
+                        <p className='text-base font-medium normal-case text-cyan-400'> Duis aute irure dolor in </p>
+                    </h2>
+                    <h2 className='text-3xl text-end font-bold uppercase'>
+                        Constellation
+                        <p className='text-base font-medium normal-case text-cyan-400'> Duis aute irure dolor in </p>
+                    </h2>
+                    <h2 className='text-3xl text-end font-bold uppercase mt-8'>
+                        Constellation
+                        <p className='text-base font-medium normal-case text-cyan-400'> Duis aute irure dolor in </p>
+                    </h2>
+                    
+                    
+                </div>
+            </div>
         </div>
     );
 };
 
 export default HomeBanner;
+
+
+
+
+// import React from 'react';
+
+// const HomeBanner = () => {
+//     return (
+//         <div>
+//             <img src="https://i.postimg.cc/vZSPsCWC/hb3.jpg" alt="" />
+//             <div>
+//                 <h2>Explore</h2>
+//                 <h1>The Universe</h1>
+//                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam, voluptatum commodi aut inventore obcaecati veniam.</p>
+//             </div>
+//         </div>
+//     );
+// };
+
+// export default HomeBanner;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-[2]

6f99e32dfe43817fd3a685a181a4053186cb4d92
sayeed083 <sayeedislam407@gmailcom>
2024-12-22 12:19:59 +0600

    Home Banner 80 Percent

--- src/Pages/Home/HomeBanner/HomeBanner.jsx
+++ src/Pages/Home/HomeBanner/HomeBanner.jsx
@@ -4,12 +4,15 @@ import './HomeBanner.css';
 const HomeBanner = () => {
     return (
         <div className="home-banner">
-            <img src="https://i.postimg.cc/vZSPsCWC/hb3.jpg" alt="Universe Background" className="background-image" />
+            {/* <img src="https://i.postimg.cc/vZSPsCWC/hb3.jpg" alt="Universe Background" className="background-image" /> */}
+            {/* <img src="https://i.postimg.cc/YS5b4gD3/hb2.jpg" alt="Universe Background" className="background-image" /> */}
+            <img src="https://i.postimg.cc/8CJZPw4c/hb1.jpg" alt="Universe Background" className="background-image" />
             <div className="banner-content mt-10 flex justify-between">
                 <div>
                     <h2 className='text-5xl font-semibold uppercase'>Explore</h2>
                     <h1 className='text-7xl font-bold uppercase my-8'>The Universe</h1>
                     <p className='text-lg font-medium'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam, <br /> voluptatum commodi aut inventore obcaecati veniam.</p>
+                    <button className="btn btn-outline btn-primary w-[200px] my-4">Get Started</button>
                 </div>
                 <div>
                     <h2 className='text-3xl text-end font-bold uppercase '>
